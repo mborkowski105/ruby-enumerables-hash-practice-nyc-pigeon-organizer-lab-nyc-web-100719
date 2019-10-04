6a35373284026d2bcda27938063e275d4c95f118
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 23:57:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 def nyc_pigeon_organizer(input_pigeon_list)
   # write your code here!
       name_array = []
-      
+      input_pigeon_list.each do |
       
       
       total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
