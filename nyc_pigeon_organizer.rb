09a8a0fd9fa4dfe94e804e67dbe43aa76e3d99c2
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 23:48:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+      pigeon_list = {
+      "Luca" => {
+        :color => ["brown"],
+        :gender => ["male"],
+        :lives => ["Library"]
+      },
+      "Lola" => {
+        :color => ["black"],
+        :gender => ["female"],
+        :lives => ["Central Park"]
+      }
+    }
+    
+    return pigeon_list
 end
