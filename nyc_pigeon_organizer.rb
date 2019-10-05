d0d9efddf21167049153e9058383eaade0a5e94b
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 00:22:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,14 +10,6 @@ def nyc_pigeon_organizer(input_pigeon_list)
       
     }
   
-  name_array = name_array.sort!
-
-  result = name_array.reject.with_index do |ele,index|
-    res = (name_array[index+1] ^ ele)
-    res == 0
-  end
-
-
-  puts result 
-  return output_pigeon_list
+  name_array.uniq!
+  puts name_array
 end
