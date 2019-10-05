a63a47bd29de6d986be41f22ff7b71619319edff
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 01:37:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,18 @@
 def nyc_pigeon_organizer(input_pigeon_list)
   # write your code here!
-    name_array = []
+    dup_name_array = []
     input_pigeon_list[:color].each do |key, value|
-      name_array.push(value)
+      dup_name_array.push(value)
     end
     
-    name_array.map
+    name_array = []
+    dup_name_array.each do |item|
+      item.each do |sub_item|
+        name_array.push(sub_item)
+      end
+    end
     
+    name_array = name_array.uniq
     
     output_pigeon_list = {}
     name_array.each do |name|
