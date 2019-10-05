b55051b0bd95374e2105074fc092bd35bc524ba2
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 00:36:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(input_pigeon_list)
   # write your code here!
     name_array = []
     input_pigeon_list[:color].each do |color|
-      if (name_array.include?)
+      if !(name_array.include? color[1])
       name_array.push(color[1])
     end
   
