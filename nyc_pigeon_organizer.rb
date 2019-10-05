651f4be02b86596a6d643f2311558718962eac7b
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 00:51:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,15 +2,19 @@ def nyc_pigeon_organizer(input_pigeon_list)
   # write your code here!
     name_array = []
     input_pigeon_list[:color].each do |color|
-      if !(name_array.include? color[1])
       name_array.push(color[1])
+    end
+  
+    output_pigeon_list = {}
+    name_array.each do |name|
+      if !(output_pigeon_list[name])
+        output_pigeon_list[name] = name => {
+          :color => [],
+          :gender => [],
+          :lives => []
+        }
       end
     end
   
-      
-    output_pigeon_list = {
-      
-    }
-  
-  puts name_array
+  puts output_pigeon_list
 end
