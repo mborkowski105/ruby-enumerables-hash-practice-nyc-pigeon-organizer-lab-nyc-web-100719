45ce8baf4de7e1449fac725bdfde6d54d6dd047d
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-05 01:29:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,8 @@ def nyc_pigeon_organizer(input_pigeon_list)
     input_pigeon_list[:color].each do |key, value|
       name_array.push(value)
     end
-  
+    
+    name_array = name_array.uniq
     output_pigeon_list = {}
     name_array.each do |name|
         output_pigeon_list[name] = name
